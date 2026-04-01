6c614df44689d04c037b28a288704dcd670660c3
yotsarunkia <yotsarun.kia@student.mahidol.edu>
2026-04-01 02:34:28 +0000

    test

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
       }
       else
       {
-        cout << "Empty queue" << endl; // ADDED!
+        cout << "Empty Queue" << endl; // ADDED!
         return 0;
       }
     }
