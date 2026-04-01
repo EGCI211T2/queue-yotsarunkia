b319a724742e59402751a92652c9381e0a1b2dcf
yotsarunkia <yotsarun.kia@student.mahidol.edu>
2026-04-01 02:32:07 +0000

    test

--- main.cpp
+++ main.cpp
@@ -3,20 +3,29 @@
 using namespace std;
 #include "Queue.h"
 
-int main(int argc , char **argv) {
+int main(int argc, char **argv)
+{
   Queue q;
-   int i,x;
-   
+  int i, x;
 
- for(i=1;i<argc;i++){
-        if(strcmp(argv[i],"x")==0){
-            x=q.dequeue();
-            if(x!=-1) cout<<"dequeing "<<x<<endl;
-        }
-        else {
-       q.enqueue(atoi(argv[i]));
-           
-        }
- }
+  for (i = 1; i < argc; i++)
+  {
+    if (strcmp(argv[i], "x") == 0)
+    {
+      x = q.dequeue();
+      if (x != -1)
+      {
+        cout << "dequeing " << x << endl;
+      }
+      else
+      {
+        cout << "Empty queue" << endl; // ADDED!
+      }
+    }
+    else
+    {
+      q.enqueue(atoi(argv[i]));
+    }
+  }
   return 0;
 }
